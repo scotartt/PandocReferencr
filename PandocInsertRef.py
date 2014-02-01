f117ea635e8defcf6a1d0449c56f79bbfecb7cb2
scotartt <scot.mcphee@gmail.com>
2014-02-01 21:04:14 +1000

    better behaviour with in inserting the footnote text at the EOF and files with no linefeed at the end of the file.

--- PandocInsertRef.py
+++ PandocInsertRef.py
@@ -3,19 +3,24 @@ import sublime, sublime_plugin, sys, re, operator
 footnote_insert = re.compile(r'(\[\^([a-zA-Z0-9]+?)\])\s*[^:]') 
 footnote_text = re.compile(r'^\s*(((\[\^([a-zA-Z0-9]+?)\])\s*:).*?)$', re.MULTILINE) 
 spaces = re.compile(r"[\s\W]+", re.MULTILINE)
+# the following is deliberately greedy. we're trying to find the newlines at EOF
+eof_newlines0 = re.compile(r'^(\s+)$', re.MULTILINE)
 
-_s = sublime.load_settings('PandocReferencr.sublime-settings')
-_footnote_placement = _s.get('footnote_placement', 'document')
-_space_prepend_count = _s.get('space_prepend_count', 0)
-_default_footnote_prefix = _s.get('default_footnote_prefix', 'fn')
-if _space_prepend_count > 2:
-	print ("space prepend count value is too large. forcing to maxvalue=2 ... was " + str(_space_prepend_count))
-	_space_prepend_count = 2;
+# global settings variables.
+_set = sublime.load_settings('PandocReferencr.sublime-settings')
+_footnote_placement = _set.get('footnote_placement', 'document')
+_space_prepend_count = _set.get('space_prepend_count', 0)
+_default_footnote_prefix = _set.get('default_footnote_prefix', 'fn')
+if _space_prepend_count > 3:
+	print ("space prepend count value is too large. forcing to maxvalue=3 ... was " + str(_space_prepend_count))
+	_space_prepend_count = 3;
+print("InsertFootnote settings complete. Footnote placement='" + str(_footnote_placement) + "'; prepended spaces='" + str(_space_prepend_count) + "'; default prefix='" + str(_default_footnote_prefix) +"'")
+# end global settings variables.
 
 class InsertFootnoteCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		sublime.status_message('Insert Reference... ')
-		print("Referencr - Insert a new Reference at " + _footnote_placement + " end with " + str(_space_prepend_count) + " prepended spaces and default prefix: " + _default_footnote_prefix)
+		print("Referencr - Insert a new Reference")
 		v = self.view
 		w = v.window()
 		r = v.sel()[0]
@@ -57,8 +62,52 @@ class InsertEnteredFnCommand(sublime_plugin.TextCommand):
 
 class InsertEnteredFootnoteTextCommand(sublime_plugin.TextCommand):
 	def run(self, edit, fnref, fntext):
-		self.view.insert(edit, self.view.size(), "\n  " + fnref + ": " + fntext + "\n")
+		(insert_pt, is_two_newlines_required) = self.calc_fn_text_insert()
+		prepend = self.get_prepend(is_two_newlines_required)
+		self.view.insert(edit, insert_pt, prepend + fnref + ": " + fntext + "\n")
 		pass
 
+	def get_prepend(self, is_two_newlines_required):
+		prepend = ""
+		if is_two_newlines_required:
+			prepend += "\n\n"
+		else:
+			prepend += "\n"
+		i = 0
+		while i < _space_prepend_count:
+			i += 1
+			prepend = prepend + " "
+		return prepend
 
-
+	def calc_fn_text_insert(self):
+		# a complex and horrible procedure to locate 
+		# the best position to insert the fn text.
+		buffer_size = self.view.size()
+		match_start = buffer_size
+		match_end = buffer_size
+		all_buffer = self.view.substr(sublime.Region(0, buffer_size))
+		#we're looking for multiple whitespace/newlines at the end of the file.
+		matches0 = eof_newlines0.finditer(all_buffer)
+		if matches0:
+			# primitively iterate over the whole thing reassigning the start and end.
+			for match in matches0:
+				(match_start, match_end) = match.span()
+				# the last one in the list is the one we want anyway.
+				print("start={0}; end={1}; eof={2}".format(match_start, match_end, buffer_size))
+			if match_end+2 < buffer_size : 
+				# we did not find the end of the file.
+				# usually because there is not a newline at the end of it.
+				# therefore simply the end of the buffer is the one we want
+				# but put two newlines at the start of the inserted note.
+				return buffer_size, True
+			else :
+				# it's the EOF region. the match start is where we begin to insert
+				# use a single newline at the start of the footnote.
+				# if you have multiple newlines at the end of your file
+				# this will insert at the TOP of the newlines.
+				# this will get confused by complex mixtures of whitespace
+				# and newlines. your EOF should be cleaner than that. ;-)
+				return match_start, False
+		else:
+			# there were no matches, just use the EOF and prepend 2 newlines.
+			return buffer_size, True
--- README.md
+++ README.md
@@ -25,7 +25,33 @@ Default command palette (and key bindings):
 
 * 'Pandoc Footnotes: Insert New' (super+alt+r)
 
-There are also menu items under "Tools - Pandoc Footnotes"
+There are also menu items under "Tools -> Pandoc Footnotes". See below for behaviour.
+
+
+Settings
+--------
+
+Select "Preferences -> Package Settings -> Pandoc Referencr -> Settings - User" to customised your settings. You can also override the keyboard bindings if you prefer other keystrokes to the ones I've provided.
+
+Here are the default settings:
+
+    {
+        // footnote_placement is either 'document' or 'paragraph'. 
+        "footnote_placement": "document",
+
+        // how many spaces to put before the footnote text. 
+        // default is 0. Must be < 4. Values other than 0 not strictly
+        // conformant to the Pandoc spec, but they work if < 4 (four spaces
+        // or a tab, or more, would make the text block into a code block)
+        "space_prepend_count": 0,
+
+        // by default, what is the default prefix to use for footnotes 
+        // when no other value can be computed. default is "fn".
+        "default_footnote_prefix": "fn",
+    }
+
+"Footnote placement" is not currently used; see below. Footnotes are currently always at the end of the document.
+
 
 Inserting Footnotes (super+alt+r)
 -------------------
@@ -34,7 +60,9 @@ Command: insert_footnote
 
 When you insert a footnote, you will be asked for the footnote id. If you have any selections, the first selection will pre-fill the footnote id field. The footnote id is inserted at the end of the selection, otherwise at the current cursor position.
 
-After you enter the footnote id, you will be asked to supply the footnote text. The footnote text is inserted at the end of the document.
+After you enter the footnote id, you will be asked to supply the footnote text. The footnote text is inserted at the end of the document. It tries to insert the footnote cleanly. If there is no newline at the end of the file, it compensates for this. If there are multiple newlines at the end of the file, it inserts the footnote at the top of the newlines. If there is a complex mixture of whitespace and newlines at the end of the file, it acts fairly sensibly, but not as cleanly. You could try keeping the end of your file in a cleaner state ;-) ... there's probably plugins for that.
+
+*N.B. I've actually only tested with 'newlines,' i.e. Unix flavour line feed ('\n')! Files with "DOS" (Windows) style line endings ('\r\n') may confuse it. I can't tell; I have no Windows and I could not be bothered creating a test file. It's pretty sad that our text files formats are still under the sway of teletypes and line printers!*
 
 There are a number of changes coming to this behaviour. Please see the comments on [this issue](https://github.com/scotartt/PandocReferencr/issues/1) for a description of how that behaviour will work.
 
@@ -47,7 +75,7 @@ This checks that each footnote id inserted in the text has a corresponding footn
 
 When you check the footnotes, if everything is OK in current buffer (open document), that is all the footnote ids and texts match, then the status line will say "All OK!"
 
-If there are errors, a warning dialog will tell you the ids and line numbers where there are issues.
+If there are errors, a warning dialog will tell you the footnote ids and line numbers where there are issues.
 
     For example, this text has a footnote with a valid footnote 
     text.[^footnote1] However this note is missing,[^missing1] if 
