08809e0b70d1bd06d5dca97e338a9e2701aea9f1
scotartt <scot.mcphee@gmail.com>
2014-05-11 18:30:44 +1000

    globals not loading correctly causing failure to insert reference plugin on reload - fixed

--- PandocInsertRef.py
+++ PandocInsertRef.py
@@ -6,19 +6,26 @@ spaces = re.compile(r"[\s\W]+", re.MULTILINE)
 # the following is deliberately greedy. we're trying to find the newlines at EOF
 eof_newlines0 = re.compile(r'^(\s+)$', re.MULTILINE)
 
-# global settings variables.
-_set = sublime.load_settings('PandocReferencr.sublime-settings')
-_footnote_placement = _set.get('footnote_placement', 'document')
-_space_prepend_count = _set.get('space_prepend_count', 0)
-_default_footnote_prefix = _set.get('default_footnote_prefix', 'fn')
-if _space_prepend_count > 3 :
-	print ("space prepend count value is too large. forcing to maxvalue=3 ... was " + str(_space_prepend_count))
-	_space_prepend_count = 3
-print("InsertFootnote settings complete. Footnote placement='" + str(_footnote_placement) + "'; prepended spaces='" + str(_space_prepend_count) + "'; default prefix='" + str(_default_footnote_prefix) +"'")
-# end global settings variables.
+_footnote_placement = 'document'
+_space_prepend_count = 0
+_default_footnote_prefix = 'fn'
+
+def get_prefs():
+	global _footnote_placement
+	global _space_prepend_count
+	global _default_footnote_prefix
+	glob_settings = sublime.load_settings('PandocReferencr.sublime-settings')
+	_footnote_placement = glob_settings.get('footnote_placement', 'document')
+	_space_prepend_count = glob_settings.get('space_prepend_count', 0)
+	_default_footnote_prefix = glob_settings.get('default_footnote_prefix', 'fn')
+	if _space_prepend_count > 3 :
+		print ("space prepend count value is too large. forcing to maxvalue=3 ... was " + str(_space_prepend_count))
+		_space_prepend_count = 3
+	print("InsertFootnote settings complete. Footnote placement='" + str(_footnote_placement) + "'; prepended spaces='" + str(_space_prepend_count) + "'; default prefix='" + str(_default_footnote_prefix) +"'")
 
 class InsertFootnoteCommand(sublime_plugin.TextCommand) :
 	def run(self, edit):
+		get_prefs()
 		sublime.status_message('Insert Reference... ')
 		print("Referencr - Insert a new Reference")
 		v = self.view
@@ -39,6 +46,7 @@ class InsertFootnoteCommand(sublime_plugin.TextCommand) :
 
 class InsertEnteredFnCommand(sublime_plugin.TextCommand) :
 	def run(self, edit, ref) :
+		get_prefs()
 		_ref = "[^" + ref + "]"
 		v = self.view
 		w = v.window()
@@ -62,6 +70,7 @@ class InsertEnteredFnCommand(sublime_plugin.TextCommand) :
 
 class InsertEnteredFootnoteTextCommand(sublime_plugin.TextCommand) :
 	def run(self, edit, fnref, fntext) :
+		get_prefs()
 		(insert_pt, is_two_newlines_required) = self.calc_fn_text_insert()
 		prepend = self.get_prepend(is_two_newlines_required)
 		self.view.insert(edit, insert_pt, prepend + fnref + ": " + fntext + "\n")
